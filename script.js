4b038d90a021e44d563b5fcf3164d754867261a8
Subha-CJPR <56904625+Subha-CJPR@users.noreply.github.com>
2023-06-22 19:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function getFormvalue(event) {
     event.preventDefault(); // Prevent the form from submitting and refreshing the page
     var firstName = document.querySelector("input[name='fname']").value;
     var lastName = document.querySelector("input[name='lname']").value;
-    alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    alert(firstName + " " + lastName);
 }
